ba6abd5176662c70cc050fd6cec7693f2a74e45d
Gobikrishnan T <68734147+GobikrishnanT@users.noreply.github.com>
2021-05-08 10:01:17 +0530

    Update mainJavaScript.js

--- mainJavaScript.js
+++ mainJavaScript.js
@@ -1,100 +1,197 @@
-//Creating div for input for user to enter their brthday date
-//1. div
-let div1 = elementCreater('div' , '' , 'input_container_cls' , 'input_container_id');
-div1.style.textAlign = "Center";
-div1.style.backgroundColor = "aqua";
-//2. Label for input
-let label_for_inputDate = elementCreater('Label' , 'Hello user' , 'label_for_input_cls' , '' , 'input_date_id');
-label_for_inputDate.style.fontSize = "25px";
-label_for_inputDate.style.fontWeight = "bolder";
-//3 . input
-let input_for_date = document.createElement('input');
-input_for_date.setAttribute('type' , 'date');
-//input_for_date.setAttribute('value' , '0');
-input_for_date.id="input_date_id";
-input_for_date.style.margin="10px";
-input_for_date.style.width = "30%";
+//div for display the status;
+let displayDiv = document.createElement('div');
+displayDiv.setAttribute('class' , 'display');
+displayDiv.style.marginRight = "50%";
+displayDiv.textContent = "__!__";
+displayDiv.style.width = "100%";
+displayDiv.style.backgroundColor = "aqua";
+displayDiv.style.height = "10vh";
+displayDiv.style.textAlign = "center";
+displayDiv.style.fontSize = "40px";
+displayDiv.style.fontFamily = "Dancing Script";
+document.body.appendChild(displayDiv);
+//let create a table and contents
+//1.create a table
+let mainTable = elementCreater('table' , '' , 'table');
+document.body.append(mainTable);
 
-//4.Creating the button for it:
-let myBtn = document.createElement('button');
-myBtn.setAttribute('type' , 'click');
-myBtn.textContent = "See it !!!";
-myBtn.style.margin = "10px";
-
-// // 5 . Create a table for show the output :
-// let myTable = document.createElement('table');
-// let mytr = document.createElement('tr');
-// myTable.append(mytr);
-
-
-//appending the child for label
-label_for_inputDate.appendChild(input_for_date);
-//Appending the label box to main box;
-div1.append(label_for_inputDate , myBtn);
-//Appending it to the body
-document.body.append(div1);
+//2 . let create a thead and tbody for heading purpose 
+let theadTr = document.createElement('tr');
+let thead = elementCreater('thead' , '' , 'thead-dark');
+let th1 = elementCreater('th' , 'ID');
+let th2 = elementCreater('th' , 'Name');
+let th3 = elementCreater('th' , 'Email-ID');
+theadTr.append(th1 , th2 , th3);
+thead.append(theadTr);
+mainTable.appendChild(thead);
 
 
-let tempdiv = document.createElement('div');
-tempdiv.style.background = "black";
-tempdiv.style.color = "white";
-tempdiv.style.fontStyle = "Times New Roman";
-div1.append(tempdiv);
-//Now create a addEvent listener for the button:
-myBtn.addEventListener('click' , (event) => {
-    tempdiv.innerHTML = "";
-    let entered_date_timeStamp = input_for_date.valueAsNumber;
-    let myDate = input_for_date.valueAsDate;
-    let div_0 = document.createElement('div');
-    div_0.innerText = myDate;
-    let age_in_MiliSeconds = Date.now() - entered_date_timeStamp;
+
+//Tag for body it is the main Tag
+let tbody = document.createElement('tbody');
+// mainTable.appendChild(tbody);
+
+
+
+
+//Function to Create any element dynamically
+function elementCreater(elementName , value='' , className='' , idName = '') {
+    let tag = document.createElement(elementName);
+    tag.setAttribute('class' , className);
+    tag.setAttribute('id' , idName);
+    tag.textContent = value;
+    return tag;
+}
+
+
+
+
+//let create a button for accesss this data
+
+for(let i = 0 ; i <= 12 ; i++) {
+
+    if(i === 10) {
+        let btn = document.createElement('button');
+        btn.setAttribute('class' , 'btn-danger');
+        btn.textContent = "First Page";
+        btn.style.margin = "5px";
+        btn.style.padding = "15px";
+        document.body.append(btn);
+    }
+
+    else if(i === 11) {
+        let btn = document.createElement('button');
+        btn.setAttribute('class' , 'btn-success  stopper');
+        btn.textContent = "Prev Page";
+        btn.style.margin = "5px";
+        btn.style.padding = "15px";
+        document.body.append(btn);
+    }
+
+    else if(i === 12) {
+        let btn = document.createElement('button');
+        btn.setAttribute('class' , 'btn-info');
+        btn.textContent = "Last Page";
+        btn.style.margin = "5px";
+        btn.style.padding = "15px";
+        document.body.append(btn);
+    }
+    else {
+        let btn = document.createElement('button');
+        btn.setAttribute('class' , 'btn-primary');
+        btn.textContent = i;
+        btn.style.margin = "5px";
+        btn.style.padding = "15px";
+        document.body.append(btn);
+    }
     
+}
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET' , 'https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json' , true);
+xhr.send();
 
 
-   let seconds = 1000 ; //1 sec = 1000ms
-   let minutes = 60 * seconds ; // for 1 minute - > 60 sec for converting to miliseconds - > * by 1000
-   let hours = 60 * minutes ; // for 1 hour = 60 minutes for convert to ms -> * seconds
-   let day = 24 * hours;
-   let month = 30 * day;
-   let year = 365 * day;
+
+let myBtn = document.querySelectorAll('button');
 
 
-   let age_in_year = Math.round(age_in_MiliSeconds / year);
-   let div_1 = document.createElement('div');
-    div_1.innerText = age_in_year;
-   
-   let age_in_months = Math.round(age_in_MiliSeconds / month);
-   let div_2 = document.createElement('div');
-    div_2.innerText = age_in_months;
- 
-   let age_in_days = Math.round(age_in_MiliSeconds / day);
-   let div_3 = document.createElement('div');
-    div_3.innerText = age_in_days;
-   
-   let age_in_hour = Math.round(age_in_MiliSeconds / hours);
-   let div_4 = document.createElement('div');
-    div_4.innerText = age_in_hour;
-   
-   let age_in_min = Math.round(age_in_MiliSeconds / minutes);
-   let div_5 = document.createElement('div');
-    div_5.innerText = age_in_min;
-   
-   let age_in_sec = Math.round(age_in_MiliSeconds / seconds);
-   let div_6 = document.createElement('div');
-    div_6.innerText = age_in_sec;
+//Function to add the content to table
+function tableAdder(val , mainData) {
+    if(val !== 'Prev Page') {
+        tbody.innerHTML = "";
+        if(val === 'First Page') {
+            for(let i = 0 ; i < 10 ; i++) {
+                let myBodyTr = document.createElement('tr');
+                for(let index in mainData[i]) {
+                    let myTd = document.createElement('td');
+                    myTd.innerText = mainData[i][index];
+                    myBodyTr.appendChild(myTd);
+                    tbody.appendChild(myBodyTr);
+                    mainTable.appendChild(tbody);
+                }
+            }
+        }
+        else if(val === 'Last Page') {
+            for(let i = 90 ; i < 101 ; i++) {
+                let myBodyTr = document.createElement('tr');
+                for(let index in mainData[i]) {
+                    let myTd = document.createElement('td');
+                    myTd.innerText = mainData[i][index];
+                    myBodyTr.appendChild(myTd);
+                    tbody.appendChild(myBodyTr);
+                    mainTable.appendChild(tbody);
+                }
+            }
+        }
+        else{
+            for(let i = val*10 ; i < (val*10) + 10 ; i++) {
+                let myBodyTr = document.createElement('tr');
+                for(let index in mainData[i]) {
+                    let myTd = document.createElement('td');
+                    myTd.innerText = mainData[i][index];
+                    myBodyTr.appendChild(myTd);
+                    tbody.appendChild(myBodyTr);
+                    mainTable.appendChild(tbody);
+                }
+            }
+        }  
+    }
+    else{
+        let prevVal = tbody.children;
+        if(prevVal.length !== 0) {
+            displayDiv.textContent = "__!__";
+            let fromVal = prevVal[0].children[0].textContent;
+            
+            if(+fromVal !== 1) {
+                tbody.innerHTML = "";
+                for(let i = fromVal-11 ; i < fromVal-1 ; i++) {
+                    let myBodyTr = document.createElement('tr');
+                    for(let index in mainData[i]) {
+                        let myTd = document.createElement('td');
+                        myTd.innerText = mainData[i][index];
+                        myBodyTr.appendChild(myTd);
+                        tbody.appendChild(myBodyTr);
+                        mainTable.appendChild(tbody);
+                    }
+                }
+            }
+            else{
+               let btn = document.querySelector('.stopper');
+               let timerId =  setInterval(() => {
+                    btn.style.backgroundColor = "Silver";
+                } , 100);
 
-    let div_miliSec_7 = document.createElement('div');
-    div_miliSec_7.innerText = age_in_MiliSeconds;
+                setTimeout(() => {
+                    clearInterval(timerId);
+                    btn.style.backgroundColor = "green";
+                } , 1000);
+                displayDiv.textContent = "No page Available Before that";
+            }
+            
+        }
+        else{
+            let btn = document.querySelector('.stopper');
+            let timerId =  setInterval(() => {
+                btn.style.backgroundColor = "Silver";
+            } , 100);
 
+            setTimeout(() => {
+                clearInterval(timerId);
+                btn.style.backgroundColor = "green";
+            } , 1000);
+            displayDiv.textContent = "No page Available Befor that";
+        }
+        
+    }
+}
 
-    // let myStyle = document.createElement('style');
-    // myStyle.innerHTML = `
-    //     .div_1 , .div_2 , .div_3 , .div_4 , .div_5 , .div_6 , .div_miliSec_7 {
-    //         background: linear-gradient(180deg , rgb(145, 248, 145) , rgb(2, 253, 2));
-    //     }
-    // `;
+for(let i = 0 ; i < myBtn.length ; i++) {
+    myBtn[i].addEventListener('click' , (event) => {
+        let mainData = JSON.parse(xhr.response);
+        let catchedVal = event.target.textContent;
+        displayDiv.textContent = "Hi, Visitor Welcome";
+        tableAdder(catchedVal , mainData);
+    });
+}
 
-    // document.head.appendChild(myStyle);
-    
-    tempdiv.append(div_0 , div_1 , div_2 , div_3 , div_4 , div_5 , div_6 , div_miliSec_7)
-});
